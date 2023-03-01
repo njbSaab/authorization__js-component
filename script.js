3f19083f2c251739ef9bfe4b174a1a4384627ea8
Pro <Admin@Pro.local>
2023-03-01 14:53:46 +0200

    noComment

---
+++ script.js
@@ -0,0 +1,105 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+   const form = document.getElementById('form')
+   form.addEventListener('submit', formSend)
+
+   async function formSend(e){
+      e.preventDefault();
+
+      let error = formValidates(form)
+
+      let formData = new FormData(form)
+      formData.append('image', formImage.files[0])
+
+      if(error === 0){
+         form.classList.add('_sending')
+
+         let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+         })
+         if(response.ok){
+            let result = await response.json();
+            alert(result.massage)
+            formPreview.innerHTML = '';
+            form.reset()
+         }else{
+            alert('error send')
+         }
+
+      } 
+      else {
+            alert('You must fill the form ')
+      }
+   }
+
+   function formValidates(e){
+      let error = 0
+      let formReq = document.querySelectorAll('._req')
+
+      for (let index = 0; index < formReq.length; index++) {
+         const input = formReq[index];
+         formRemoveError(input)
+
+         if(input.classList.contains('_email')){
+            if(emailTest(input)){
+               formAddError(input)
+               error++;
+               console.log("error");
+            }
+         }else if(input.getAttribute("type") === "checkbox" &&
+          input.checked === false){
+             formAddError(input)
+             error++;
+             console.log("error");
+          }else{
+             if(input.value === ''){
+                error++
+                console.log("error");
+             }
+          }
+      }
+   }
+
+   function formAddError(input){
+      input.parentElement.classList.add('_error')
+      input.classList.add('_error')
+   }
+   function formRemoveError(input){
+      input.parentElement.classList.remove('_error')
+      input.classList.remove('_error')
+   }
+
+
+function emailTest(input) {
+  return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
+}
+
+   const formImage = document.getElementById('formImage')
+   const formPreview = document.getElementById('formPreview')
+
+   formImage.addEventListener('change', () =>{
+      uploadFile(formImage.files[0])
+   })
+
+   function uploadFile(file){
+      if(!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)){
+         alert('permission added only pictures')
+         formImage.value = "";
+         return
+      }
+      if(file.size > 2 * 1024 * 1024){
+         alert('fail must less 2 mb')
+         return
+      }
+     let reader = new FileReader()
+     reader.onload = function (e) {
+        formPreview.innerHTML = `<img src="${e.target.result}" alt="Photo">`
+     }
+     reader.onerror = function(e){
+        alert('Error')
+     }
+     reader.readAsDataURL(file)
+   }
+})
